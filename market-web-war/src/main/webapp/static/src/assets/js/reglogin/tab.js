dcc5a8aa86cfa724cd2e76ad2e2aac6b1e642353
dengshaofei <dengsf007@gmail.com>
2016-04-01 10:54:35 +0800

    commit static

--- market-web-war/src/main/webapp/static/src/assets/js/reglogin/tab.js
+++ market-web-war/src/main/webapp/static/src/assets/js/reglogin/tab.js
@@ -1,6 +1,11 @@
+var UI ={
+    $tabContent:$('.tab-content')
+}
+
 $('#header-title').text('注册');
 $('.tab-list').on('click','.tab', function(){
     $(this).addClass('active').siblings().removeClass('active');
     var index = $(this).index();
-    $('.tab-content').eq(index).addClass('active');
+    UI.$tabContent.removeClass('active').eq(index).addClass('active');
 });
+$('.tab-list .tab').eq(0).click();
