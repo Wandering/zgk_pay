499fa1965220221e79a6b897bb1438f351969080
dengshaofei <dengsf007@gmail.com>
2016-04-06 01:30:24 +0800

    static

--- market-web-war/src/main/webapp/static/dist/js/commons.js
+++ market-web-war/src/main/webapp/static/dist/js/commons.js
@@ -96,9 +96,9 @@
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var domainStr = 'zhigaokao.cn'; //正式
+	var domainStr = 'm.zhigaokao.cn'; //正式
 	//var domainStr = 'test.zhigaokao.cn'; //测试
-	//var domainStr = 'zhigaokao.com:8084';
+	//var domainStr = 'm.zhigaokao.com:8084';
 
 	//获取域名前缀=============================
 	var urlDomain = window.location.hostname + '';
