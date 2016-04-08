831c79dbad7352ddd8d302076db91ed912f9b6b0
dengshaofei <dengsf007@gmail.com>
2016-04-08 18:04:42 +0800

    static

--- market-web-war/src/main/webapp/static/dist/js/vipCheck.js
+++ market-web-war/src/main/webapp/static/dist/js/vipCheck.js
@@ -46,22 +46,22 @@
 
 	var urlConfig = __webpack_require__(3);
 	var cookie = __webpack_require__(2);
-	var getTime = __webpack_require__(4);
 	var token = cookie.getCookieValue('token');
 	var isLogin = cookie.getCookieValue('isLogin');
+
 	$(function () {
 	    $('#header-title').text('VIP');
 	    $('#header-menu').show();
 	    var vipActiveDate = cookie.getCookieValue('vipActiveDate');
 	    var vipEndDate = cookie.getCookieValue('vipEndDate');
-	    var vipActiveDateV = getTime(vipActiveDate,'yyyy-MM-dd').substr(0, 10);
-	    var vipEndDateV = getTime(vipEndDate,'yyyy-MM-dd').substr(0, 10);
-	    $('#startDate').text(vipActiveDateV);
-	    $('#endDate').text(vipEndDateV);
+	    $('#startDate').text(vipActiveDate);
+	    $('#endDate').text(vipEndDate);
 	    if(isLogin){
 	        $('#vip-buy').attr('href','/vip-buy?token='+token);
 	    }
 	});
+
+
 
 
 
@@ -363,39 +363,6 @@
 
 
 	module.exports = interfaceUrl;
-
-
-/***/ },
-/* 4 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_RESULT__ = function () {
-	//时间戳转换
-	    Date.prototype.Format = function (fmt) {
-	        var o = {
-	            "M+": this.getMonth() + 1, //月份
-	            "d+": this.getDate(), //日
-	            "h+": this.getHours(), //小时
-	            "m+": this.getMinutes(), //分
-	            "s+": this.getSeconds(), //秒
-	            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-	            "S": this.getMilliseconds() //毫秒
-	        };
-	        if (/(y+)/.test(fmt))
-	            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-	        for (var k in o)
-	            if (new RegExp("(" + k + ")").test(fmt))
-	                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-	        return fmt;
-	    };
-	    var getTime = function (timestamp,formatStr) {
-	        var newDate = new Date();
-	        newDate.setTime(timestamp);
-	        return newDate.Format(formatStr || "yyyy-MM-dd hh:mm:ss");
-	    };
-
-	    return getTime;
-	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
 
 
 /***/ }
--- market-web-war/src/main/webapp/static/src/assets/js/vip-check/vipCheck.js
+++ market-web-war/src/main/webapp/static/src/assets/js/vip-check/vipCheck.js
@@ -1,21 +1,21 @@
 var urlConfig = require('urlConfig');
 var cookie = require('cookie');
-var getTime = require('timeFormat');
 var token = cookie.getCookieValue('token');
 var isLogin = cookie.getCookieValue('isLogin');
+
 $(function () {
     $('#header-title').text('VIP');
     $('#header-menu').show();
     var vipActiveDate = cookie.getCookieValue('vipActiveDate');
     var vipEndDate = cookie.getCookieValue('vipEndDate');
-    var vipActiveDateV = getTime(vipActiveDate,'yyyy-MM-dd').substr(0, 10);
-    var vipEndDateV = getTime(vipEndDate,'yyyy-MM-dd').substr(0, 10);
-    $('#startDate').text(vipActiveDateV);
-    $('#endDate').text(vipEndDateV);
+    $('#startDate').text(vipActiveDate);
+    $('#endDate').text(vipEndDate);
     if(isLogin){
         $('#vip-buy').attr('href','/vip-buy?token='+token);
     }
 });
+
+
 
 
 
