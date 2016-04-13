8dff68e77ff2bad3589c45c2a3ec07361e7f6df1
liuxiaowen <liuxiaowen@thinkjoy.cn>
2016-04-13 15:54:08 +0800

    update

--- market-web-war/src/main/java/cn/thinkjoy/zgk/market/interceptor/LoginInterceptor.java
+++ market-web-war/src/main/java/cn/thinkjoy/zgk/market/interceptor/LoginInterceptor.java
@@ -25,26 +25,41 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String url = request.getServletPath();
-        String token = request.getParameter("token");
+        String queryStr = request.getQueryString();
         String toUrl = request.getParameter("toUrl");
-        String redirectUrl = "login";
-        if (StringUtils.isEmpty(token) && !ServletPathConst.MAPPING_URLS.contains(url)) {
-            return true;
-        } else if (!StringUtils.isEmpty(token) && !ServletPathConst.MAPPING_URLS.contains(url)) {
-            return true;
-        } else if (StringUtils.isEmpty(token) && ServletPathConst.MAPPING_URLS.contains(url)) {
-            response.sendRedirect(redirectUrl + "?toUrl=" + toUrl);
-            throw new BizException("0000001", "请登陆后再操作!");
+        if(!StringUtils.isEmpty(queryStr))
+        {
+            url += "?" + queryStr + "&";
         }
+        if(!StringUtils.isEmpty(toUrl))
+        {
+            if(url.indexOf("?") == -1)
+            {
+                url += "?";
+            }
+            url  += "toUrl=" +toUrl;
+        }
+        response.sendRedirect(url);
 
-        String key = UserRedisConst.USER_KEY + token;
-        boolean redisFlag = RedisUtil.getInstance().exists(key);
-        if (redisFlag) {
-            return true;
-        } else if (ServletPathConst.MAPPING_URLS.contains(url)) {
-            response.sendRedirect(redirectUrl + "?toUrl=" + toUrl);
-            throw new BizException("0000001", "请登陆后再操作!");
-        }
+//        String token = request.getParameter("token");
+//        String redirectUrl = "login";
+//        if (StringUtils.isEmpty(token) && !ServletPathConst.MAPPING_URLS.contains(url)) {
+//            return true;
+//        } else if (!StringUtils.isEmpty(token) && !ServletPathConst.MAPPING_URLS.contains(url)) {
+//            return true;
+//        } else if (StringUtils.isEmpty(token) && ServletPathConst.MAPPING_URLS.contains(url)) {
+//            response.sendRedirect(redirectUrl + "?toUrl=" + toUrl);
+//            throw new BizException("0000001", "请登陆后再操作!");
+//        }
+//
+//        String key = UserRedisConst.USER_KEY + token;
+//        boolean redisFlag = RedisUtil.getInstance().exists(key);
+//        if (redisFlag) {
+//            return true;
+//        } else if (ServletPathConst.MAPPING_URLS.contains(url)) {
+//            response.sendRedirect(redirectUrl + "?toUrl=" + toUrl);
+//            throw new BizException("0000001", "请登陆后再操作!");
+//        }
         return true;
     }
 
