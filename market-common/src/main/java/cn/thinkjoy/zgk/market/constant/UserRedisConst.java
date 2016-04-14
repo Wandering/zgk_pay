ca527b0c4a6cf1b892d4380e5091db9924d42edd
liuxiaowen <liuxiaowen@thinkjoy.cn>
2016-04-14 19:16:40 +0800

    update

--- market-common/src/main/java/cn/thinkjoy/zgk/market/constant/UserRedisConst.java
+++ market-common/src/main/java/cn/thinkjoy/zgk/market/constant/UserRedisConst.java
@@ -6,5 +6,8 @@ public class UserRedisConst {
 	 * 用户信息
 	 */
 	public static final String USER_KEY = "zgk_user_";
-	
+
+	public static final Long ACCESS_TOKEN_EXPIRE_TIME = 7000l;
+
+	public static final Long TICKET_EXPIRE_TIME = 7000l;
 }
