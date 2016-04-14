95bd356835b71c7a3a937d6a645c49ac6afdaa15
dengshaofei <dengsf007@gmail.com>
2016-04-14 21:24:41 +0800

    static

--- market-web-war/src/main/webapp/static/src/assets/js/order/order.js
+++ market-web-war/src/main/webapp/static/src/assets/js/order/order.js
@@ -172,9 +172,7 @@
             if (res.rtnCode == '0000000') {
                 var charge = res.bizData;
                 charge.credential = JSON.parse(charge.credential);
-                alert(11)
                 pingpp.createPayment(charge, function(result, error){
-                    alert(JSON.stringify(error))
                     if (result == "success") {
                         // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的 wap 支付结果都是在 extra 中对应的 URL 跳转。
                         orderPayStatus('支付成功');
