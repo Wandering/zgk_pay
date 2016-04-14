54ef912e6952617417fef63ef4b80d4f308abee2
pdeng <pdeng@thinkjoy.cn>
2016-04-14 23:48:13 +0800

    bug修复 .

--- market-web-war/src/main/webapp/static/dist/js/userDetail.js
+++ market-web-war/src/main/webapp/static/dist/js/userDetail.js
@@ -112,9 +112,12 @@ webpackJsonp([14],[
 	    }
 
 	    function getOpenId(code) {
+	        alert(interfaceUrl.getOpenId)
 	            util.ajaxFun(interfaceUrl.getOpenId, 'get', {
 	                code: code
 	            }, function (res) {
+
+	                alert(8)
 	                alert(JSON.stringify(res))
 	                if (res.rtnCode == '0000000') {
 	                    cookie.setCookie("openId", res.bizData.openId, 4, "/");
--- market-web-war/src/main/webapp/static/src/assets/js/user-detail/user-detail.js
+++ market-web-war/src/main/webapp/static/src/assets/js/user-detail/user-detail.js
@@ -108,9 +108,12 @@ $(function(){
     }
 
     function getOpenId(code) {
+        alert(interfaceUrl.getOpenId)
             util.ajaxFun(interfaceUrl.getOpenId, 'get', {
                 code: code
             }, function (res) {
+
+                alert(8)
                 alert(JSON.stringify(res))
                 if (res.rtnCode == '0000000') {
                     cookie.setCookie("openId", res.bizData.openId, 4, "/");
