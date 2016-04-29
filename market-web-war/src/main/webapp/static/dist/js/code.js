297befa0ded33219dd8abff89351dadac3d0462f
dengshaofei <dengsf007@gmail.com>
2016-04-29 15:03:23 +0800

    Merge branch 'develop'
    * develop:
      static
      static
    
    # Conflicts:
    #	market-web-war/src/main/webapp/static/dist/js/code.js
    #	market-web-war/src/main/webapp/static/src/assets/js/code/code.js

--- market-web-war/src/main/webapp/static/dist/js/code.js
+++ market-web-war/src/main/webapp/static/dist/js/code.js
@@ -149,13 +149,9 @@ webpackJsonp([1],[
 	        $.getJSON('/pay/getAccessToken', function (res) {
 	            if (res.rtnCode == "0000000") {
 	                var ticket = res.bizData.ticket;
-<<<<<<< HEAD
 	                var string1 = "jsapi_ticket=" + ticket + "&noncestr=" + noncestr + "&timestamp=" + timestamp + "&url=http://zgkser.zhigaokao.cn/vip?state=vip";
 	                //var string1 = "jsapi_ticket=" + ticket + "&noncestr=" + noncestr + "&timestamp=" + timestamp + "&url="+window.location.href;
-=======
-	                //var string1 = "jsapi_ticket=" + ticket + "&noncestr=" + noncestr + "&timestamp=" + timestamp + "&url="+window.location.href;
-	                var string1 = "jsapi_ticket=" + ticket + "&noncestr=" + noncestr + "&timestamp=" + timestamp + "&url=http://zgkser.zhigaokao.cn/vip?state=vip";
->>>>>>> develop
+
 	                //alert(string1)
 
 	                var sign = CryptoJS.SHA1(string1);
