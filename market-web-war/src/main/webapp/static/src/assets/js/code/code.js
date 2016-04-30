a509f377623e5f3a84372d68525abb68ae5f384b
dengshaofei <dengsf007@gmail.com>
2016-04-30 12:25:16 +0800

    Merge branch 'develop'
    * develop:
      commit  zhifu
      commit  zhifu
    
    # Conflicts:
    #	market-web-war/src/main/webapp/static/dist/js/vipBuy.js
    #	market-web-war/src/main/webapp/static/src/assets/js/vip-buy/vip-buy.js

--- market-web-war/src/main/webapp/static/src/assets/js/code/code.js
+++ market-web-war/src/main/webapp/static/src/assets/js/code/code.js
@@ -130,7 +130,7 @@ $(function () {
 
     var noncestr = 'U5iQqjfV123NT5du';
 
-    var  urls = 'http://zgkser.zhigaokao.cn/vip?state=vip';
+    var  urls = 'http://zgkser.zhigaokao.cn/vip?state=vip&userId='+userId;
 
     function getSign() {
         $.ajaxSettings.async = false;
@@ -182,8 +182,8 @@ $(function () {
         wx.onMenuShareAppMessage({
             title: title,
             desc: desc,
-            link: window.location.href,//分享链接
-            //link: urls,//分享链接
+            //link: window.location.href,//分享链接
+            link: urls,//分享链接
             imgUrl: logo, // 分享图标
             trigger: function (res) {
                 //alert('用户点击发送给朋友');
@@ -202,8 +202,8 @@ $(function () {
         wx.onMenuShareTimeline({
             title: title,
             desc: desc,
-            link: window.location.href,//分享链接
-            //link: urls,//分享链接
+            //link: window.location.href,//分享链接
+            link: urls,//分享链接
             imgUrl: logo, // 分享图标
             trigger: function (res) {
                 //alert('用户点击分享到朋友圈');
