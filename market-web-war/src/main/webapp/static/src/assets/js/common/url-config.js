7bda539a01c6d718ea722047f7d7f3c16c4d9cf0
dengshaofei <dengsf007@gmail.com>
2016-06-03 09:26:14 +0800

    commit

--- market-web-war/src/main/webapp/static/src/assets/js/common/url-config.js
+++ market-web-war/src/main/webapp/static/src/assets/js/common/url-config.js
@@ -2,8 +2,8 @@
  * url配置文件
  * */
 //var BASE_URL = 'http://s1.service.zhigaokao.cn/'; //正式
-//var BASE_URL = 'http://dev.service.zhigaokao.cn/';  //正式环境
-var BASE_URL = 'http://172.16.160.73:8066/';  //测试环境
+var BASE_URL = 'http://dev.service.zhigaokao.cn/';  //正式环境
+//var BASE_URL = 'http://172.16.160.73:8066/';  //测试环境
 //var BASE_URL = 'http://172.16.160.31:8080';  //小文本地
 //var BASE_URL = 'http://172.16.160.82:8085';  //小文本地
 //var BASE_URL = 'http://172.16.160.72:8089';  //左浩本地
